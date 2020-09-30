fff5e8559ea2b4c91e2fd415a4b5f7d31721a15e
Bruno Reis <reisfmb@gmail.com>
2020-09-30 12:08:32 -0300

    Improved Exercise 5.1

--- Chap5/Exercises.thy
+++ Chap5/Exercises.thy
@@ -10,25 +10,25 @@ lemma
   and TA: "\<forall> x y. T x y \<longrightarrow> A x y"
   and AA: "A x y"
 shows "T x y"
-proof(cases)
-  assume a1:"T y x"
-  from this and TA have "A y x" by blast
-  from this and AA and A have a2:"x = y" by blast
-  from this and a1 have "T x y" by simp
-  from this show "T x y" by auto
-next
-  assume a1:"\<not> T y x"
-  from this and T show "T x y" by blast
-qed
-
-
-text \<open> A more direct solution...
 proof(rule ccontr)
   assume a1:"\<not> T x y"
   from this and T have a2:"T y x" by blast
   from this and TA have "A y x" by blast
   from this and A and AA have "x = y" by blast
   from this and a1 and a2 show "False" by blast 
+qed
+
+
+
+text \<open> A more direct solution...
+proof(cases)
+  assume a1:"T y x"
+  from this and TA have "A y x" by auto
+  from this and AA and A have a2:"x = y" by auto
+  from this and a1 show "T x y" by simp
+next
+  assume a1:"\<not> T y x"
+  from this and T show "T x y" by auto
 qed
 \<close>
 
